0639142e9bc886859a270f86c66c526765205771
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-26 20:50:07 +0330

    validation class for validation

---
+++ src/main/java/com/example/final_project_part3_springmvc/utility/ValidationCode.java
@@ -0,0 +1,17 @@
+package com.example.final_project_part3_springmvc.utility;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ValidCode.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidationCode {
+    String message() default "Invalid nation code";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+}
