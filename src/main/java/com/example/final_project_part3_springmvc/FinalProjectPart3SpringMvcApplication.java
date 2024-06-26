36fa8f73c66f21425b4eabd33335044eb8c05c95
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-26 19:19:47 +0330

    first commit for final project part 3 spring mvc

---
+++ src/main/java/com/example/final_project_part3_springmvc/FinalProjectPart3SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_project_part3_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectPart3SpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectPart3SpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/final_project_part3_springmvc/FinalProjectPart3SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_project_part3_springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectPart3SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
