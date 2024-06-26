4b59e4c7321bc11845b245ac7d5dae692595b799
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-27 01:25:30 +0330

    add admin controller

---
+++ src/main/java/com/example/final_project_part3_springmvc/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.example.final_project_part3_springmvc.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class AdminController {
+}
