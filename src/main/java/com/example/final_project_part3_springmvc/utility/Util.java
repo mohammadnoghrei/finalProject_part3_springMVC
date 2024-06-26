a4d4bbcf1340b6fc5a05dd11cc59c9a6f6251bd1
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-26 20:49:46 +0330

    create util class

---
+++ src/main/java/com/example/final_project_part3_springmvc/utility/Util.java
@@ -0,0 +1,75 @@
+package com.example.final_project_part3_springmvc.utility;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.Objects;
+import java.util.Set;
+@Component
+public class Util {
+    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    Validator validator = validatorFactory.getValidator();
+
+
+    public boolean validate(Objects entity) {
+
+        Set<ConstraintViolation<Objects>> violations = validator.validate(entity);
+        if (violations.isEmpty())
+            return true;
+        else {
+            System.out.println("Invalid user data found:");
+            for (ConstraintViolation<Objects> violation : violations) {
+                System.out.println(violation.getMessage());
+            }
+            return false;
+        }
+    }
+
+    public static byte[] saveImage(String path) {
+        File file1 = new File(path);
+        if (isJPEG(new File(path)) && file1.length() < 300000) {
+            File file = new File(path);
+            InputStream is = null;
+            try {
+                is = new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+            byte[] bytes = new byte[(int) file.length()];
+            try {
+                is.read(bytes);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            return bytes;
+        }
+
+        return null;
+    }
+
+    private static Boolean isJPEG(File filename) {
+        boolean flag = false;
+        DataInputStream ins;
+        try {
+            ins = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            flag = ins.readInt() == 0xffd8ffe0;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                ins.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return flag;
+    }
+}
