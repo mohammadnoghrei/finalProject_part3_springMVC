427c28662976fdd3452eb090961af48bc071c354
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-27 01:23:42 +0330

    add values offer dto's

--- src/main/java/com/example/final_project_part3_springmvc/dto/offer/OfferSaveRequest.java
+++ src/main/java/com/example/final_project_part3_springmvc/dto/offer/OfferSaveRequest.java
@@ -1,4 +1,11 @@
 package com.example.final_project_part3_springmvc.dto.offer;
 
-public record OfferSaveRequest() {
+import com.example.final_project_part3_springmvc.dto.expert.ExpertSaveResponse;
+import com.example.final_project_part3_springmvc.dto.order.OrderSaveResponse;
+
+public record OfferSaveRequest(
+        OrderSaveResponse order,
+        ExpertSaveResponse expert,
+        double price,
+        boolean confirmed) {
 }
--- src/main/java/com/example/final_project_part3_springmvc/dto/offer/OfferSaveResponse.java
+++ src/main/java/com/example/final_project_part3_springmvc/dto/offer/OfferSaveResponse.java
@@ -1,4 +1,11 @@
 package com.example.final_project_part3_springmvc.dto.offer;
 
-public record OfferSaveResponse() {
+import com.example.final_project_part3_springmvc.dto.expert.ExpertSaveResponse;
+import com.example.final_project_part3_springmvc.dto.order.OrderSaveResponse;
+
+public record OfferSaveResponse(long id,
+                                OrderSaveResponse order,
+                                ExpertSaveResponse expert,
+                                double price,
+                                boolean confirmed) {
 }
