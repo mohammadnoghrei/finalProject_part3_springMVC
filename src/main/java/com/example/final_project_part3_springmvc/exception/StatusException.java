0aaabfbb956cf02953722ee9b65ce163d4ff9b92
mohammadnoghrei <noghreimohammad@gmail.com>
2024-06-26 20:52:18 +0330

    create exception

---
+++ src/main/java/com/example/final_project_part3_springmvc/exception/StatusException.java
@@ -0,0 +1,6 @@
+package com.example.final_project_part3_springmvc.exception;
+public class StatusException extends RuntimeException {
+    public StatusException(String message) {
+    super(message);
+    }
+}
