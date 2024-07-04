435b9fdf6722389c5cfe2dda15c513f24da64564
mohammadnoghrei <noghreimohammad@gmail.com>
2024-07-04 19:25:11 +0330

    wallet controller

---
+++ src/main/java/com/example/final_project_part3_springmvc/controller/WalletController.java
@@ -0,0 +1,24 @@
+package com.example.final_project_part3_springmvc.controller;
+
+import com.example.final_project_part3_springmvc.dto.offer.OfferSaveResponse;
+import com.example.final_project_part3_springmvc.dto.wallet.WalletSaveResponse;
+import com.example.final_project_part3_springmvc.mapper.OfferMapper;
+import com.example.final_project_part3_springmvc.mapper.WalletMapper;
+import com.example.final_project_part3_springmvc.service.WalletService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class WalletController {
+
+private final WalletService walletService;
+    @GetMapping("/get-by-id-wallet/{id}")
+    public WalletSaveResponse getWalletById(@PathVariable Long id) {
+        return WalletMapper.INSTANCE.modelToWalletSaveResponse(walletService.findById(id));
+    }
+}
