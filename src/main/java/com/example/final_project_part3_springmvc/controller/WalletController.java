aa84e4e7aecad3508c8fbac4e0ae868427f46df4
mohammadnoghrei <noghreimohammad@gmail.com>
2024-07-12 14:40:19 +0330

    add wallet controller and mapper

--- src/main/java/com/example/final_project_part3_springmvc/controller/WalletController.java
+++ src/main/java/com/example/final_project_part3_springmvc/controller/WalletController.java
@@ -1,19 +1,19 @@
 package com.example.final_project_part3_springmvc.controller;
 
-import com.example.final_project_part3_springmvc.dto.offer.OfferSaveResponse;
 import com.example.final_project_part3_springmvc.dto.wallet.WalletSaveResponse;
-import com.example.final_project_part3_springmvc.mapper.OfferMapper;
 import com.example.final_project_part3_springmvc.mapper.WalletMapper;
 import com.example.final_project_part3_springmvc.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/wallet")
 public class WalletController {
 
 private final WalletService walletService;
