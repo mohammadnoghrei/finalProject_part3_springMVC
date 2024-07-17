c3ccc180705c450005b237cf16dbb8b45e4dc48f
mohammadnoghrei <noghreimohammad@gmail.com>
2024-07-18 00:30:22 +0330

    add method to repo

--- src/main/java/com/example/final_project_part3_springmvc/repository/CustomerRepository.java
+++ src/main/java/com/example/final_project_part3_springmvc/repository/CustomerRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long>,JpaSpecificationExecutor{
@@ -18,4 +20,9 @@ public interface CustomerRepository extends JpaRepository<Customer,Long>,JpaSpec
     @Modifying()
     @Query("UPDATE Customer c SET c.password = :password WHERE c.username = :username")
     void updatePassword(String password,String username);
+
+//    @Query("SELECT u FROM Customer u JOIN FETCH u.orderList o GROUP BY u HAVING COUNT(o) > :orderCount")
+    @Query("SELECT c FROM Customer c WHERE SIZE(c.orderList) > :orderCount")
+    List<Customer> findCustomersWithOrderCount(@Param("orderCount") int orderCount);
+
 }
