b6a3f7500e620ca74958698e4ff38ac8bfaf72ec
mohammadnoghrei <noghreimohammad@gmail.com>
2024-07-17 18:47:52 +0330

    OrderSaveRequest fix date

--- src/main/java/com/example/final_project_part3_springmvc/dto/order/OrderSaveRequest.java
+++ src/main/java/com/example/final_project_part3_springmvc/dto/order/OrderSaveRequest.java
@@ -2,11 +2,14 @@ package com.example.final_project_part3_springmvc.dto.order;
 
 import com.example.final_project_part3_springmvc.dto.customer.CustomerSaveResponse;
 import com.example.final_project_part3_springmvc.dto.subservices.SubServicesSaveResponse;
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 
 public record OrderSaveRequest(CustomerSaveResponse customer,
                                SubServicesSaveResponse subServices,
                                double customerOfferPrice,
-                               LocalDate requestedDateToDoOrder,
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+                               LocalDateTime requestedDateToDoOrder,
                                String address) {
 }
