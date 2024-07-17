0a46ee714d14a8c111ab92a9cf0c60bd89b6207e
mohammadnoghrei <noghreimohammad@gmail.com>
2024-07-17 18:45:57 +0330

    order repo extend spicification

--- src/main/java/com/example/final_project_part3_springmvc/repository/OrderRepository.java
+++ src/main/java/com/example/final_project_part3_springmvc/repository/OrderRepository.java
@@ -1,10 +1,8 @@
 package com.example.final_project_part3_springmvc.repository;
 
-import com.example.final_project_part3_springmvc.model.Expert;
-import com.example.final_project_part3_springmvc.model.Order;
-import com.example.final_project_part3_springmvc.model.OrderStatus;
-import com.example.final_project_part3_springmvc.model.SubServices;
+import com.example.final_project_part3_springmvc.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,11 +10,13 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OrderRepository  extends JpaRepository<Order,Long> {
+public interface OrderRepository  extends JpaRepository<Order,Long>, JpaSpecificationExecutor {
 
     List<Order> findAllByOrderStatusOrOrderStatusAndSubServices(OrderStatus orderStatus1, OrderStatus orderStatus2, SubServices subServices);
     List<Order> findAllByExpertAndOrderStatus(Expert expert, OrderStatus orderStatus);
     @Modifying(clearAutomatically = true,flushAutomatically = true)
     @Query("UPDATE Order c SET c.orderStatus =  :status WHERE c.id = :id")
     void updateOrderStatus(OrderStatus status, long id);
+
+    List<Order> findAllByCustomerAndOrderStatus(Customer customer,OrderStatus orderStatus);
 }
