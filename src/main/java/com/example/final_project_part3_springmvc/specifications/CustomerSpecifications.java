8b5dca84cfc088d4d5c9ef1c98aea294d3e8dc48
mohammadnoghrei <noghreimohammad@gmail.com>
2024-07-18 00:32:22 +0330

    add date and order to customer and expert spicification

--- src/main/java/com/example/final_project_part3_springmvc/specifications/CustomerSpecifications.java
+++ src/main/java/com/example/final_project_part3_springmvc/specifications/CustomerSpecifications.java
@@ -1,23 +1,33 @@
 package com.example.final_project_part3_springmvc.specifications;
 
+import com.example.final_project_part3_springmvc.dto.customer.CustomerCriteriaDto;
 import com.example.final_project_part3_springmvc.model.Customer;
 import org.springframework.data.jpa.domain.Specification;
-
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.criteria.Predicate;
 
 public class CustomerSpecifications {
-    public static Specification<Customer> getCustomerSpecification(String firstname, String lastName, String email) {
+    public static Specification<Customer> getCustomerSpecification( CustomerCriteriaDto customerCriteriaDto) {
         return (root, query, criteriaBuilder) ->{ List<Predicate> predicates =new ArrayList<>();
-            if (firstname!=null&& !firstname.isEmpty())
-                predicates.add( criteriaBuilder.like(root.get("firstname"),firstname));
-            if (lastName!=null&& !lastName.isEmpty())
-                predicates.add( criteriaBuilder.like(root.get("lastname"),lastName));
-            if (email!=null&& !email.isEmpty())
-                predicates.add( criteriaBuilder.like(root.get("email"),email));
+
+            if (customerCriteriaDto.firstname!=null&& !customerCriteriaDto.firstname.isEmpty())
+                predicates.add( criteriaBuilder.like(root.get("firstname"),customerCriteriaDto.firstname));
+            if (customerCriteriaDto.lastname!=null&& !customerCriteriaDto.lastname.isEmpty())
+                predicates.add( criteriaBuilder.like(root.get("lastname"),customerCriteriaDto.lastname));
+            if (customerCriteriaDto.email!=null&& !customerCriteriaDto.email.isEmpty())
+                predicates.add( criteriaBuilder.like(root.get("email"),customerCriteriaDto.email));
+            if (customerCriteriaDto.orderCount>0)
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(criteriaBuilder.size(root.get("orderList")), customerCriteriaDto.orderCount));
+            if (customerCriteriaDto.minRegisterDate!=null)
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("registerDate"),customerCriteriaDto.minRegisterDate));
+            if (customerCriteriaDto.maxRegisterDate!=null)
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("registerDate"),customerCriteriaDto.maxRegisterDate));
+
+
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
+
 
 }
--- src/main/java/com/example/final_project_part3_springmvc/specifications/ExpertSpecifications.java
+++ src/main/java/com/example/final_project_part3_springmvc/specifications/ExpertSpecifications.java
@@ -1,5 +1,6 @@
 package com.example.final_project_part3_springmvc.specifications;
 
+import com.example.final_project_part3_springmvc.dto.expert.ExpertCriteriaDto;
 import com.example.final_project_part3_springmvc.model.Expert;
 import com.example.final_project_part3_springmvc.model.SubServiceExpert;
 import jakarta.persistence.criteria.Join;
@@ -12,22 +13,29 @@ import java.util.List;
 
 public class ExpertSpecifications {
 
-    public static Specification<Expert> getExpertSpecification(String firstname, String lastName, String email,int rate,String subServiceName) {
+    public static Specification<Expert> getExpertSpecification(ExpertCriteriaDto expertCriteriaDto) {
         return (root, query, criteriaBuilder) ->{
             List<Predicate> predicates =new ArrayList<>();
 
-            if (firstname!=null&& !firstname.isEmpty())
-                predicates.add( criteriaBuilder.like(root.get("firstname"),firstname));
-            if (lastName!=null&& !lastName.isEmpty())
-                predicates.add( criteriaBuilder.like(root.get("lastname"),lastName));
-            if (email!=null&& !email.isEmpty())
-                predicates.add( criteriaBuilder.like(root.get("email"),email));
-            if (rate>=0&&rate<=5)
-                predicates.add( criteriaBuilder.greaterThanOrEqualTo(root.get("avgScore"),rate));
-            if (subServiceName!=null&& !subServiceName.isEmpty()) {
+            if (expertCriteriaDto.firstname!=null&& !expertCriteriaDto.firstname.isEmpty())
+                predicates.add( criteriaBuilder.like(root.get("firstname"),expertCriteriaDto.firstname));
+            if (expertCriteriaDto.lastname!=null&& !expertCriteriaDto.lastname.isEmpty())
+                predicates.add( criteriaBuilder.like(root.get("lastname"),expertCriteriaDto.lastname));
+            if (expertCriteriaDto.email!=null&& !expertCriteriaDto.email.isEmpty())
+                predicates.add( criteriaBuilder.like(root.get("email"),expertCriteriaDto.email));
+            if (expertCriteriaDto.rate>0&&expertCriteriaDto.rate<=5)
+                predicates.add( criteriaBuilder.greaterThanOrEqualTo(root.get("avgScore"),expertCriteriaDto.rate));
+            if (expertCriteriaDto.subServiceName!=null&& !expertCriteriaDto.subServiceName.isEmpty()) {
                 Join<Expert, SubServiceExpert> expertSubServiceJoin = root.join("subServiceExperts", JoinType.INNER);
-                criteriaBuilder.equal(expertSubServiceJoin.get("subServices").get("name"), subServiceName);
+                criteriaBuilder.equal(expertSubServiceJoin.get("subServices").get("name"), expertCriteriaDto.subServiceName);
             }
+            if (expertCriteriaDto.orderCount>0)
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(criteriaBuilder.size(root.get("orderList")), expertCriteriaDto.orderCount));
+            if (expertCriteriaDto.minRegisterDate!=null)
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("registerDate"),expertCriteriaDto.minRegisterDate));
+            if (expertCriteriaDto.maxRegisterDate!=null)
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("registerDate"),expertCriteriaDto.maxRegisterDate));
+
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
